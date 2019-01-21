44b443e4dc3358d9d8a2758a8337162a590e33e2
Tropicalrambler <alainber@gmail.com>
2019-01-21 05:41:41 -0600

    insert new item and create uom if not existent and force all item groups

--- jsonshare/api.py
+++ jsonshare/api.py
@@ -58,8 +58,7 @@ def crud(item):
                                          fieldname=['item_code', 'item_name',
                                                     'item_group', 'stock_uom',
                                                     'standard_rate', 'description',
-                                                    'is_stock_item', 'opening_stock'], as_dict=1)
-
+                                                    'is_stock_item'], as_dict=1)
         compartir_data(item_data)
     except:
         frappe.msgprint(_('FAIL'))
@@ -69,14 +68,33 @@ def mensaje():
 
 def guardar_dato_recibido(item_fields):
     # frappe.msgprint(_(item_fields))
-    frappe.doc({'doctype': 'UOM','uom_name': 'palito1','must_be_whole_number': 0}).insert(ignore_permissions=True)
-    return 200
+    for item in item_fields:
+        new_item = frappe.new_doc("Item")
+        new_item.item_code = _(item.get('item_code'))
+        new_item.item_name = _(item.get('item_name'))
+        new_item.description = _(item.get('description'))
+        new_item.is_stock_item = _(item.get('is_stock_item'))
+
+        if item.get('item_group') != 'All Item Groups':
+            new_item.item_group = 'All Item Groups'
+        else:
+            new_item.item_group = _(item.get('item_group'))
+        
+        if not frappe.db.exists('UOM', _(item.get('stock_uom'))):
+            new_stock_uom = frappe.new_doc("UOM")
+            new_stock_uom.uom_name = _(item.get('stock_uom'))
+            new_stock_uom.save(ignore_permissions=True)
+        else:
+            new_item.stock_uom = _(item.get('stock_uom'))
+        new_item.save(ignore_permissions=True)
+    hello = 'Se ha creado un nuevo Item'
+    return hello
+    #return 200
 
 @frappe.whitelist(allow_guest=True)
 def receivejson(data):
-    new_item = frappe.new_doc("UOM")
-    new_item.uom_name = 'Palito1'
-    new_item.must_be_whole_number = 0
-    new_item.save(ignore_permissions=True)
-    hello = 'Hello World'
+    item_data = json.loads(data)
+    guardar_dato_recibido(item_data)
+    #frappe.publish_realtime(event='global',message='Alguien llamo este metodo de receive json',room=None)
+    hello = 'Se ha creado un nuevo Item'
     return hello
