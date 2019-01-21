f701e373da27d877a2fb216f9991e19d01a67ea5
Tropicalrambler <alainber@gmail.com>
2019-01-21 00:50:55 -0600

    adding unit tests for api

---
+++ jsonshare/test_api.py
@@ -0,0 +1,11 @@
+import unittest
+import frappe
+import api
+
+class TestApi(unittest.TestCase):
+	def test_sumoftwo_integers(self):
+		# Compruebe que la funcion hace lo que dice que hace
+		# por ejemplo:
+        a = 56
+        b = 44
+		self.assertAlmostEqual(sumtwo(a,b), a+b)
