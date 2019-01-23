8cf6c64182b9d4550174bb7dd00d344a304a1b17
monroy95 <m.monroyc22@gmail.com>
2019-01-22 22:57:45 -0600

    Modifiaciones exitosas compartir item

--- jsonshare/api.py
+++ jsonshare/api.py
@@ -74,30 +74,39 @@ def mensaje():
 
 
 def guardar_dato_recibido(item_fields):
-    n_item = len(item_fields)
+    # Guarda el numero de items recibidos a crear
+    n_item = 0
+    n_uom = 0
+    mensaje = ''
 
-    # for item in item_fields:
-    #     if not frappe.db.exists('Item', _(data.get('item_code'))):
-    #         new_item = frappe.new_doc('Item')
-    #         new_item.item_code = _(item.get('item_code'))
-    #         new_item.item_name = _(item.get('item_name'))
-    #         new_item.description = _(item.get('description'))
-    #         new_item.is_stock_item = _(item.get('is_stock_item'))
+    for item in item_fields:
+        if not frappe.db.exists('Item', _(item.get('item_code'))):
+            n_item = len(item_fields)
+            new_item = frappe.new_doc('Item')
+            new_item.item_code = _(item.get('item_code'))
+            new_item.item_name = _(item.get('item_name'))
+            new_item.description = _(item.get('description'))
+            new_item.is_stock_item = _(item.get('is_stock_item'))
 
-    #         if item.get('item_group') != 'All Item Groups':
-    #             new_item.item_group = 'All Item Groups'
-    #         else:
-    #             new_item.item_group = _(item.get('item_group'))
+            if item.get('item_group') != 'All Item Groups':
+                new_item.item_group = 'All Item Groups'
+            else:
+                new_item.item_group = _(item.get('item_group'))
 
-    #         if not frappe.db.exists('UOM', _(item.get('stock_uom'))):
-    #             new_stock_uom = frappe.new_doc("UOM")
-    #             new_stock_uom.uom_name = _(item.get('stock_uom'))
-    #             new_stock_uom.save(ignore_permissions=True)
-    #         else:
-    #             new_item.stock_uom = _(item.get('stock_uom'))
-    #             new_item.save(ignore_permissions=True)
-    # return results
-    return str(n_item)
+            if not frappe.db.exists('UOM', _(item.get('stock_uom'))):
+                new_stock_uom = frappe.new_doc("UOM")
+                new_stock_uom.uom_name = _(item.get('stock_uom'))
+                new_stock_uom.save(ignore_permissions=True)
+                n_uom += 1
+            else:
+                new_item.stock_uom = _(item.get('stock_uom'))
+            
+            new_item.save(ignore_permissions=True)
+            mensaje = '{0} Items creados, {1} UOM creados'.format(n_item, n_uom)
+        else:
+            mensaje = 'Item compartido ya existe'
+
+    return mensaje
 
 
 @frappe.whitelist(allow_guest=True)
